688884e9b8e921a5f12e421ed92e8e63517ffc55
kaLeidoscop3 <laxamanakyle@gmail.com>
2024-02-22 15:06:40 -0500

    lane_image method
    canny - computes strongest gradients in image

--- lanes.py
+++ lanes.py
@@ -1,10 +1,14 @@
 import cv2
 import numpy as np
 
+def canny(lane_image):
+    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0) 
+    canny = cv2.Canny(blur, 50, 150) # outline strongest gradients in image
+    return canny
+
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
-canny = cv2.Canny(blur, 50, 150)
+canny = canny()
 cv2.imshow("result", canny)
 cv2.waitKey(0)
