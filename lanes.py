05486d0677a06cbb12566131b0cfc8353cfcc68d
kaLeidoscop3 <laxamanakyle@gmail.com>
2024-02-22 15:37:24 -0500

    Update lanes.py
    mask to create the road image to show a specific portion of image

--- lanes.py
+++ lanes.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 def canny(lane_image):
     gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
@@ -8,9 +7,18 @@ def canny(lane_image):
     canny = cv2.Canny(blur, 50, 150) # outline strongest gradients in image
     return canny
 
+# image can be read as an array of pixels
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([
+    [(200, height ), (1100, height), (550, 250)]
+    ])
+    mask = np.zeros_like(image) # same amount of pixels and dimensions as canny image
+    cv2.fillPoly(mask, polygons, 255)
+    return mask
+
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 canny_image = canny(lane_image)
-
-plt.imshow(canny_image)
-plt.show()
+cv2.imshow("result", region_of_interest(canny_image))
+cv2.waitKey(0)
